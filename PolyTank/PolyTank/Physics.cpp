75b622d177bb734a018e50ea43368c6540ef2952
Willz789 <williammistarz@gmail.com>
2021-04-14 17:30:17 +0200

    #9

--- PolyTank/PolyTank/Physics.cpp
+++ PolyTank/PolyTank/Physics.cpp
@@ -1,1 +1,12 @@
 #include "Physics.h"
+
+using namespace DirectX;
+
+void Physics::update(float t, float dt)
+{
+	for (RigidBody* pr : rigidBodies) {
+		XMVECTOR gravity = XMVectorSet(0.0f, pr->mass*-g, 0.0f, 0.0f);
+		pr->addForce(gravity);
+		pr->update(dt);
+	}
+}
--- PolyTank/PolyTank/Physics.h
+++ PolyTank/PolyTank/Physics.h
@@ -6,9 +6,11 @@
 
 class Physics {
 public:
+	static constexpr float g = 9.82f;
 	void update(float t, float dt);
 
 private:
 	std::vector<RigidBody*> rigidBodies;
+	
 };
 
